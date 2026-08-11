1033f4068ff0bd39417a4c4a3a518327c70fbc4e
ElectroDigital853 <arjunaaronj@gmail.com>
2026-08-11 14:52:06 +0530

    Complete writing 4bitadd module

--- ee2016_26/4-bit-adder.v
+++ ee2016_26/4-bit-adder.v
@@ -1,1 +1,11 @@
-module fourbitadd(s3, s2, s1, s0, cout, a, b, cin)
+module fourbitadd(s3, s2, s1, s0, cout, a3, a2, a1, a0, b3, b2, b1, b0, cin);
+  input a3, a2, a1, a0, b3, b2, b1, b0, cin;
+  output s3, s2, s1, s0, cout;
+  wire c0, c1, c2, c3;
+
+  fourbitadd f0(s0, c0, cin, a, b);
+  fourbitadd f1(s1, c1, c0, a, b);
+  fourbitadd f2(s2, c2, c1, a, b);
+  fourbitadd f3(s3, c3, c2, a, b);
+
+endmodule;
