486a37b64b2240d8e05e2979f208454c1690cb42
ElectroDigital853 <arjunaaronj@gmail.com>
2026-09-01 16:06:27 +0530

    This may succeed
    Removed old code and implemented interrupt service routine for INT1.

--- Expt5Failed/main.asm
+++ Expt5Failed/main.asm
@@ -7,28 +7,70 @@
 
 
 ; Replace with your application code
-.CSEG
-  LDI R20,0xFF
- OUT DDRD,R20
+.ORG 0x0000 RJMP reset
+.ORG 0x0004 RJMP int1_ISR ; if not working then use 0x0004; int0 is 0x000(1 or 2).
+.ORG 0x0100
 
- LDI R20,00
- OUT PORTD,R20
+reset:
+	LDI R16, 0x70
+	OUT SPL, R16
+	LDI R16, 0x00 
+	OUT SPH, R16
+	
+	LDI R16, 0x01; 
+	OUT DDRB, R16
+	
+	LDI R16, 0x00
+	OUT DDRD, R16
+	
+	IN R16, MCUCR
+	ORI R16, 0x08 ; 0x03 for INT0
+	OUT MCUCR, R16
+	
+	IN R16, GICR
+	ORI R16, 0x80; 0x40 for INT0
+	OUT GICR, R16
+	
+	LDI R16, 0x00
+	OUT PORTB, R16
+	
+	SEI
 
-		LDI R16,0XFF
-  loop2:LDI R17,0XFF
-  loop3:DEC R17
-		BRNE loop3
+
+
+; MCUCR, GICR, Set inputs and outputs.
+
+Waiter:RJMP Waiter; Waits endlessly until an interrupt is given
+
+int1_ISR:
+	IN R16, SREG
+	PUSH R16
+	LDI R16, 0x0A
+	MOV R0, R16
+	c1:
+	LDI R16, 0x01
+	OUT PORTB, R16
+
+	LDI R16, 0xFF
+	a1: LDI R17, 0xFF
+	a2: DEC R17
+		BRNE a2
+	DEC R16
+	BRNE a1
+
+	LDI R16, 0x00
+	OUT PORTB, R16
+
+	LDI R16, 0xFF
+	b1: LDI R17, 0xFF
+	b2: DEC R17
+		BRNE b2
 		DEC R16
-		BRNE loop2
+		BRNE b1
 
-		LDI R20,0xFF
-		OUT PORTD,R20
+	DEC R0
+	BRNE c1
+	POP R16
+	OUT SREG, R16
 
-		LDI R16,0XFF
-  loop4:LDI R17,0XFF
-  loop5:DEC R17
-		BRNE loop5
-		DEC R16
-		BRNE loop4
- 
- NOP
+	RETI
